8c7db7dbb5f68d5788f2a3774ece617fee9b8fba
Nikita <kaska0093@inbox.ru>
2024-01-18 23:35:55 -0800

    addControllec + CellModel

---
+++ MVVM_CoreData/Views/SummaryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SummaryTableViewCell.swift
+//  MVVM_CoreData
+//
+//  Created by Nikita Shestakov on 18.01.2024.
+//
+
+import UIKit
+
+class SummaryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MVVM_CoreData/Views/ToDoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoTableViewCell.swift
+//  MVVM_CoreData
+//
+//  Created by Nikita Shestakov on 18.01.2024.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
