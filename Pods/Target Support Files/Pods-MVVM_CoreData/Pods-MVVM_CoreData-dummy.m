610133a8a209a80e0a1f2761d06b3c640c337bbe
Nikita <kaska0093@inbox.ru>
2024-02-19 22:29:14 -0800

    pods update

--- Pods/Target Support Files/Pods-MVVM_CoreData/Pods-MVVM_CoreData-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MVVM_CoreData : NSObject
-@end
-@implementation PodsDummy_Pods_MVVM_CoreData
-@end
--- Pods/Target Support Files/Pods-Task Manager/Pods-Task Manager-umbrella.h
+++ Pods/Target Support Files/Pods-Task Manager/Pods-Task Manager-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_MVVM_CoreDataVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MVVM_CoreDataVersionString[];
+FOUNDATION_EXPORT double Pods_Task_ManagerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Task_ManagerVersionString[];
 
